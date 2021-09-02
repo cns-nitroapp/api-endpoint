6beaa3907bc5b228dc775a8e36274d8010560d43
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-02 21:40:26 +0200

    Implementation of GitHub API
    Creation of .md files on GitHub using PyGithub

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import logging
 import json
+from github import Github
 
 from flask.wrappers import Response
 
@@ -14,35 +15,32 @@ def post():
     data = request.get_json()
     agent = request.headers.get('User-Agent')
 
-    try:
-        if ('authorization' in data):
-            if (data['authorization'] == 'demo'):
-                if ('id' in data):
-                    if (type(data['id']) == int):
-                        if (len(data['title']) >= 1):
-                            if (len(data['description']) >= 1):
-                                if (len(data['start-date']) >= 1):
-                                    if (len(data['end-date']) >= 1):
-                                        write(data, getagent(agent))
-                                        return { "status": 200, "content": "OK" }, 200
-                                    else:
-                                        return { "status": 403, "type": "invalid.end.date" }, 403
+    if ('authorization' in data):
+        if (data['authorization'] == 'demo'):
+            if ('id' in data):
+                if (type(data['id']) == int):
+                    if (len(data['title']) >= 1):
+                        if (len(data['description']) >= 1):
+                            if (len(data['start-date']) >= 1):
+                                if (len(data['end-date']) >= 1):
+                                    write(data, getagent(agent))
+                                    return { "status": 200, "content": "OK" }, 200
                                 else:
-                                    return { "status": 403, "type": "invalid.start.date" }, 403
+                                    return { "status": 403, "type": "invalid.end.date" }, 403
                             else:
-                                return { "status": 403, "type": "invalid.description" }, 403
+                                return { "status": 403, "type": "invalid.start.date" }, 403
                         else:
-                            return { "status": 403, "type": "invalid.title" }, 403
+                            return { "status": 403, "type": "invalid.description" }, 403
                     else:
-                        return { "status": 403, "type": "invalid.post.id" }, 403
+                        return { "status": 403, "type": "invalid.title" }, 403
                 else:
-                    return { "status": 403, "type": "missing.post.id" }, 403
+                    return { "status": 403, "type": "invalid.post.id" }, 403
             else:
-                return { "status": 400, "type": "invalid.api.key" }, 400
+                return { "status": 403, "type": "missing.post.id" }, 403
         else:
-            return { "status": 403, "type": "missing.api.key" }, 403
-    except:
-        return { "status": 500, "content": "Request cancelled - An error has occured. Please contact us at contact@constellate.pro"}, 500
+            return { "status": 400, "type": "invalid.api.key" }, 400
+    else:
+        return { "status": 403, "type": "missing.api.key" }, 403
 
 def getagent(agent):
     if (agent == 'got (https://github.com/sindresorhus/got)'):
@@ -54,9 +52,38 @@ def getagent(agent):
 
 
 def write(data, agent):
-    with open('../_posts/' + str(data['id']) + '.md', 'w') as fp:            
-            fp.write('---\nlayout: post\ntitle: "' + data['title'] + '"\ndescription: "' + data['description'] + '"\ntags: [' + agent + ']\n---')
+    with open('../_posts/' + str(data['id']) + '.md', 'w') as fp:
+        content = '---\nlayout: post\ntitle: "' + data['title'] + '"\nauthor: [' + agent + ']\n---\n' + data['description']
+        fp.write(content)
+        github(str(data['id']), content)
 
+def github(id, content):
+    g = Github('') #PERSONAL_ACCESS TOKEN HERE
 
+    org = g.get_organization('cns-nitroapp')
+    org.login
+    repo = org.get_repo('blog')
+
+    all_files = []
+    contents = repo.get_contents("")
+    while contents:
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        else:
+            file = file_content
+            all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+
+    # Upload to github
+    git_prefix = '_posts/'
+    git_file = git_prefix + '2021-09-02-' + id + '.md'
+    if git_file in all_files:
+        contents = repo.get_contents(git_file)
+        repo.update_file(contents.path, "committing files", content, contents.sha, branch="main")
+        print(git_file + ' UPDATED')
+    else:
+        repo.create_file(git_file, "committing files", content, branch="main")
+        print(git_file + ' CREATED')
+        
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
