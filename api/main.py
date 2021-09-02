ea877da6f506b92957499c93e5a6443b7acfa54d
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-09-02 18:09:20 +0200

    Add error handling (Post ID, Title, Description)

--- api/main.py
+++ api/main.py
@@ -13,22 +13,28 @@ app = Flask(__name__)
 def post():
     data = request.get_json()
 
-    try:
-        if ('authorization' in data):
-            if (data['authorization'] == 'demo'):
-                if ('id' in data):
-                    with open('../_posts/' + data['id'] + '.json', 'w') as jsonFile:
-                        json.dump(data, jsonFile)
-                    print(data)
-                    return data
+    if ('authorization' in data):
+        if (data['authorization'] == 'demo'):
+            if ('id' in data):
+                if (type(data['id']) == int):
+                    if (len(data['title']) >= 1):
+                        if (len(data['description']) >= 1):
+                            with open('../_posts/' + str(data['id']) + '.json', 'w') as jsonFile:
+                                json.dump(data, jsonFile)
+                            print(data)
+                            return { "status": 200, "content": "OK" }, 200
+                        else:
+                            return { "status": 403, "type": "invalid.description" }, 403
+                    else:
+                        return { "status": 403, "type": "invalid.title" }, 403
                 else:
-                    return { "status": "403", "response": "Request cancelled - Missing Post ID" }, 403
+                    return { "status": 403, "type": "invalid.post.id" }, 403
             else:
-                return { "status": "400", "response": "Request cancelled - Invalid API Key" }, 400
+                return { "status": 403, "type": "missing.post.id" }, 403
         else:
-            return { "status": "403", "response": "Request cancelled - No API Key" }, 403
-    except:
-        return { "status": "500", "response": "Request cancelled - An error has occured. Please contact us at contact@constellate.pro"}, 500
+            return { "status": 400, "type": "invalid.api.key" }, 400
+    else:
+        return { "status": 403, "type": "missing.api.key" }, 403
 
 
 if __name__ == "__main__":
